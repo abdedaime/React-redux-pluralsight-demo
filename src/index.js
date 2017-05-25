105359d277c9ab8335921e81de79676869a47c27
hicham abdedaime <habdedaime@gmail.com>
2017-05-25 12:19:32 +0100

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 /**
  * Created by habdedaime on 24/05/2017.
  */
-console.log('hi');
+//console.log('hi');
---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import colors from  'colors';
+
+/**
+ * Created by habdedaime on 24/05/2017.
+ */
+//console.log("Starting app in dev mode .....".green)
